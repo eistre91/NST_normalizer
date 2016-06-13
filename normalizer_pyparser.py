37ebfb397d5c21fe97b6aa7ec400ec65a0ff34a8
Erik Istre <e.istre91@gmail.com>
2016-06-14 01:38:26 +1200

    Finish full normalization for lambda reductions

--- normalizer_pyparser.py
+++ normalizer_pyparser.py
@@ -1,10 +1,11 @@
 from pyparsing import *
+from copy import deepcopy
 
 """
 	Will attempt to normalize given lambda terms for NNST. 
 	
 	Syntax:
-		Lambdas are to be written as L    		['L', variable, expr]
+		Lambdas are to be written as (Lx.M)    		['L', variable, expr]
 		Application 							['A', expr, expr]
 		
 		Universal Quantification as U    		['U', L-var, expr]
@@ -52,10 +53,12 @@ dot = Literal(".")
 expr << ( \
 		var | \
 		Group(lparens + expr + expr + rparens).setParseAction(lambda x: x[0].insert(0,'A')) | \
-		lparens + Group( lam('rule') + var('variable') + dot.suppress() + expr('expr')) + rparens
+		lparens + Group( lam + var + dot.suppress() + expr ) + rparens
 		)
 		
 #Group(lparens + expr + expr + rparens).setParseAction(lambda x: x[0].insert(0,'A')) | \
+
+
 
 # Unparses expressions and returns the string representation
 # Takes in the full parsed expression list at index 0
@@ -68,8 +71,90 @@ def unparse(parsed_expr):
 	elif parsed_expr[0] == 'L':
 		expr_string = '(' + 'L' + parsed_expr[1] + '.' + unparse(parsed_expr[2]) + ')'
 	return expr_string
-#print(' '.join(expr.parseString('(ab)').asList()[0]))		
-		
+#print(' '.join(expr.parseString('(ab)').asList()[0]))
+
+#Lambda Reduction: ['A', ['L', var, expr0], expr1]
+#Reduces to: expr1 subbed for all occurrences of var in expr0	
+#Algorithm: traverse expr0 and find all occurrences of var and replace with expr1
+def check_lambda_reduction(parsed_expr):
+	if parsed_expr[0] == 'A' and parsed_expr[1][0] == 'L':
+		return True
+
+# Takes in the full parsed expression list at index 0	
+# Will perform a single reduction step at the first reduction it finds
+def step_reduce(parsed_expr):
+	'''Check to see if a reduction can be performed at the current level
+		If so, do it 
+		Return reduced expr as parsed list'''
+	result = []
+	
+	if check_lambda_reduction(parsed_expr):
+		var = parsed_expr[1][1]
+		primary_expr = parsed_expr[1][2]
+		secondary_expr = parsed_expr[2]
+		result = substitute(var, primary_expr, secondary_expr)
+	
+	return result
+
+# Traverse the parsed expression and find a reduction
+# Can't guarantee much about what order this algorithm proceeds, 
+# but it will find all reductions
+def find_reduction(parsed_expr):
+	"""If you find reduction at the top level, do that one.
+	Else go looking until you find one in a subtree, or reach a var."""
+	result = []
+	if isinstance(parsed_expr, str):
+		return parsed_expr
+	elif check_lambda_reduction(parsed_expr):
+		var = parsed_expr[1][1]
+		primary_expr = parsed_expr[1][2]
+		secondary_expr = parsed_expr[2]
+		result = substitute(var, primary_expr, secondary_expr)		
+		return result
+	else: 
+		if parsed_expr[0] == 'A':
+			result = ['A', find_reduction(parsed_expr[1]), find_reduction(parsed_expr[2])]
+			return result
+		elif parsed_expr[0] == 'L':
+			result = ['L', var, find_reduction(parsed_expr[2])]
+			return result
+		else:
+			return parsedExpr
+			
+def normalize(parsed_expr, max_iterations = 100):
+	iterations = 1
+	pre_reduced = parsed_expr
+	reduced = find_reduction(parsed_expr)
+	print("Iteration:", iterations, "\n current expression:", reduced)
+	while iterations < max_iterations and pre_reduced != reduced:
+		pre_reduced = reduced
+		reduced = find_reduction(reduced)
+		iterations += 1
+		print("Iteration:", iterations, "\n current expression:", reduced)
+	return reduced
+	
+# Traverse the parsed expression to find all instances of var
+# in primary expr to be replaced by secondary_expr
+def substitute(var, primary_expr, secondary_expr):
+	result = None
+	if primary_expr == var:
+		result = secondary_expr
+	elif isinstance(primary_expr, list):
+		result = deepcopy(primary_expr)
+		for i, piece in enumerate(primary_expr):
+			if piece == var:
+				result[i] = secondary_expr
+			elif isinstance(piece, list):
+				result[i] = substitute(var, piece, secondary_expr)
+			else:
+				pass
+	else: 
+		pass
+	return result
+	
+# This is definitely not the most efficient program 
+# possible due to re-scanning and many many other redundancies.
+
 # Test code
 if __name__ == "__main__":
 	print()
@@ -99,5 +184,40 @@ if __name__ == "__main__":
 	print(test_parse)
 	print(unparse(test_parse[0].asList()) == test_string)
 	print()
+	
+	test_string = '((Lx.x)y)'
+	test_parse = expr.parseString(test_string)
+	print(test_parse)
+	reduction_result = step_reduce(test_parse[0].asList())
+	print(reduction_result)
+	reduction_result = find_reduction(test_parse[0].asList())
+	print(reduction_result)
+	print()
+	
+	test_string = '(((Ly.(Lx.(xy)))a)b)'
+	test_parse = expr.parseString(test_string)
+	print(test_parse)
+	#reduction_result = step_reduce(test_parse[0].asList())
+	#print(reduction_result)
+	reduction_result = find_reduction(test_parse[0].asList())
+	print(reduction_result)
+	reduction_result = normalize(test_parse[0].asList())
+	print(reduction_result)
+	print()
+	
+	test_string = '((Lx.((xx)(cd)))(Ly.y))'
+	test_parse = expr.parseString(test_string)
+	print(test_parse)
+	test_string_reduced = '(((Ly.y)(Ly.y))(cd))'
+	test_parse_reduced = expr.parseString(test_string_reduced)
+	print(test_parse_reduced)
+	reduction_result = step_reduce(test_parse[0].asList())
+	print(reduction_result)
+	normalize_result = normalize(test_parse[0].asList())
+	print(normalize_result)
+	print(unparse(reduction_result))
+	print(unparse(normalize_result))
+	print()
+
 	
 # Couldn't figure out how to make results names work.
